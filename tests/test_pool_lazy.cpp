34e8bc83ead325d4a90c93a70f7f5930be4ada55
tzcnt <trailingzerocount@gmail.com>
2025-11-09 15:00:45 -0800

    destructor_count_test for multiple sizes

--- tests/test_pool_lazy.cpp
+++ tests/test_pool_lazy.cpp
@@ -11,7 +11,7 @@
 class CATEGORY : public testing::Test {
 protected:
   static void SetUpTestSuite() {
-    tmc::cpu_executor().set_thread_count(1).init();
+    tmc::cpu_executor().set_thread_count(16).init();
   }
 
   static void TearDownTestSuite() { tmc::cpu_executor().teardown(); }
@@ -35,27 +35,36 @@ public:
         BitmapObjectPoolImpl<destructor_counter, DestructorCounterPool>() {}
 };
 
-TEST_F(CATEGORY, destructor_count) {
-  test_async_main(ex(), []() -> tmc::task<void> {
+template <size_t Count> void destructor_count_test(tmc::ex_cpu& ex) {
+  test_async_main(ex, []() -> tmc::task<void> {
     std::atomic<size_t> destroyed_count = 0;
     {
       DestructorCounterPool pool{destroyed_count};
-      tmc::barrier bar(63);
+      tmc::barrier bar(Count);
 
-      auto tasks = std::ranges::views::iota(0, 63) |
-                   std::ranges::views::transform([&](int i) -> tmc::task<void> {
-                     return [](
-                              DestructorCounterPool& Pool, tmc::barrier& Bar
-                            ) -> tmc::task<void> {
-                       auto obj = Pool.acquire();
-                       co_await Bar;
-                       co_return;
-                     }(pool, bar);
-                   });
+      auto tasks =
+        std::ranges::views::iota(0UL, Count) |
+        std::ranges::views::transform([&](size_t i) -> tmc::task<void> {
+          return [](
+                   DestructorCounterPool& Pool, tmc::barrier& Bar
+                 ) -> tmc::task<void> {
+            auto obj = Pool.acquire();
+            co_await Bar;
+            co_return;
+          }(pool, bar);
+        });
       co_await tmc::spawn_many(tasks);
     }
-    EXPECT_EQ(destroyed_count, 63);
+    EXPECT_EQ(destroyed_count.load(), Count);
   }());
 }
+
+TEST_F(CATEGORY, destructor_count_0) { destructor_count_test<0>(ex()); }
+TEST_F(CATEGORY, destructor_count_1) { destructor_count_test<1>(ex()); }
+TEST_F(CATEGORY, destructor_count_63) { destructor_count_test<63>(ex()); }
+TEST_F(CATEGORY, destructor_count_64) { destructor_count_test<64>(ex()); }
+TEST_F(CATEGORY, destructor_count_127) { destructor_count_test<127>(ex()); }
+TEST_F(CATEGORY, destructor_count_128) { destructor_count_test<128>(ex()); }
+TEST_F(CATEGORY, destructor_count_9999) { destructor_count_test<9999>(ex()); }
 
 #undef CATEGORY
